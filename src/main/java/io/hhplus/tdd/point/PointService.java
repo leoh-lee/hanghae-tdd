b71d947af726b5a8f3ac1a7629b8b356f076e4cf
nimoh <spakers38@gmail.com>
2024-12-15 17:18:27 +0900

    Feat: 사용자 ID로 포인트 충전/이용 내역을 조회하는 기능 구

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,16 +1,24 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint getUserPointByUserId(long id) {
         return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> getPointHistoryByUserId(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
