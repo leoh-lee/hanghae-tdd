2b22bca60bd3b02f4469a5dafdc354e7106d1ecd
nimoh <spakers38@gmail.com>
2024-12-15 18:10:22 +0900

    Feat: 특정 유저의 포인트를 사용하는 기능 구현
    - UserPoint에 point가 충분한 지 검사하는 메서드 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -25,4 +25,14 @@ public class PointService {
     public UserPoint chargeUserPoint(long userId, long amount) {
         return userPointTable.insertOrUpdate(userId, amount);
     }
+
+    public UserPoint usePoint(long userId, long amount) {
+        UserPoint userPoint = userPointTable.selectById(userId);
+
+        if (userPoint.isNotEnoughPoints(amount)) {
+            throw new IllegalStateException("포인트가 부족하여 사용할 수 없습니다.");
+        }
+
+        return userPointTable.insertOrUpdate(userId, userPoint.point() - amount);
+    }
 }
